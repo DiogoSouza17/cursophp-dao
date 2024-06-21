3c7b48be6252e8d710929a78f0600d0311efbeca
Diogo Souza <diogo-97@hotmail.com>
2024-06-21 10:03:59 -0300

    iniciamos o projeto

---
+++ class/sql.php
@@ -0,0 +1,48 @@
+<?php
+
+class sql extends PDO {
+
+	private $conn;
+
+	public function __construct (){
+
+		$this->conn = new PDO ("mysql:dbname=dbphp; host=localhost", "root", "");
+	}
+
+	private function setParams ($statment, $parameters = array()) {
+
+		foreach ($parameters as $key => $value) {
+			
+			$this->setParam($key, $value);
+		}
+	}
+
+	private function setParam ($statment, $key, $value) {
+
+		$statment->bindParam ($key, $value);
+	}
+
+	public function execquery ($rawQuery, $params = array ()) {
+
+		// chamei o query. $rawQuery = é query bruta. 
+        //$params = dados que estarão na query(independente de qual query seja) 
+
+		$stmt = $this->conn->prepare($rawQuery); // preparando a query.
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt;
+	}
+
+	public function select ($rawQuery, $params = array ()):array {
+
+		$stmt = $this->execquery($rawQuery, $params);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+}
+
+?>
